58a211b70d9ee4d35f11d3aaa08a6ce52393c24e
wilbertverayin <wilbertverayin@gmail.com>
2015-09-01 18:29:51 +0800

    replaced stopwords filter implementation

--- ngram.py
+++ ngram.py
@@ -1,27 +1,22 @@
-from datetime import datetime
-
 import csv
 import re
 import gzip
 import nltk
 
-STEMMED_WORDS = nltk.corpus.stopwords.words('english')
+STOPWORDS = nltk.corpus.stopwords.words('english')
 SNOWBALL_STEMMER = nltk.stem.SnowballStemmer('english')
 NEWLINE_CONSTANT = 'ada770804a0b11e5885dfeff819cdc9f'
 unigram_cache = []
 
 def main_program():
-    sentence_list = list(open('input/input.txt', 'r'))
+    with open('input/input.txt', 'r') as f:
+        sentence_list = f.readlines()
 
-    print('Type of sentence_list:')
-    print(type(sentence_list))
+    input_string = join_sentence_list(sentence_list)
 
-    get_ngram_for_string(sentence_list, 1, True)
-    get_ngram_for_string(sentence_list, 2, True)
-    get_ngram_for_string(sentence_list, 3, True)
-
-def list_subtraction(minuend, subtrahend):
-    return [item for item in minuend if item not in subtrahend]
+    get_ngram_for_string(input_string, 1, True)
+    get_ngram_for_string(input_string, 2, True)
+    get_ngram_for_string(input_string, 3, True)
 
 def get_unigrams(sentence, get_base_words=False):
     global unigram_cache
@@ -29,10 +24,9 @@ def get_unigrams(sentence, get_base_words=False):
         tokens = nltk.word_tokenize(sentence)
 
         if get_base_words:
-            # tokens = [SNOWBALL_STEMMER.stem(token) for token in tokens]
-            tokens = map(SNOWBALL_STEMMER.stem, tokens)
+            unigram_cache = [SNOWBALL_STEMMER.stem(token) for token in tokens]
 
-        unigram_cache = list_subtraction(tokens, STEMMED_WORDS)
+        unigram_cache = tokens
 
     return unigram_cache
 
@@ -56,13 +50,17 @@ def tag_cloud_to_file(tag_cloud, filename, sort_input=False):
         writer.writerows(tag_cloud)
 
 def join_sentence_list(sentence_list):
+    stopwords_pattern = re.compile(r'\b(' + r'|'.join(STOPWORDS) + r')\b\s*')
+    whitespace_pattern = re.compile(r'([^\s\w]|_)+')
+
     combined_string = (' ' + str(NEWLINE_CONSTANT) + ' ').join(sentence_list)
-    combined_string = re.sub(r'([^\s\w]|_)+', '', combined_string)
     combined_string = combined_string.lower()
+    combined_string = stopwords_pattern.sub(' ', combined_string)
+    combined_string = whitespace_pattern.sub('', combined_string)
+
     return combined_string
 
-def get_ngram_for_string(sentence_list, ntype, get_base_words):
-    input_string = join_sentence_list(sentence_list)
+def get_ngram_for_string(input_string, ntype, get_base_words):
     if ntype == 1:
         ngrams = get_unigrams(input_string, get_base_words)
     elif ntype == 2:
@@ -84,6 +82,4 @@ def get_ngram_for_string(sentence_list, ntype, get_base_words):
     tag_cloud_to_file(ngram_no_newlines, output_path)
 
 if __name__ == '__main__':
-    startTime = datetime.now()
     main_program()
-    print (datetime.now() - startTime)
